7ddea2005be04201ee9244dfe158d15b010a501d
George <george@pop-os.localdomain>
2020-05-01 12:57:41 +0100

    implements listeners vs Consumer for efficiency

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -21,7 +21,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context, listen: true);
+    final product = Provider.of<Product>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -39,12 +39,14 @@ class ProductItem extends StatelessWidget {
         header: null,
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavourite ? Icons.favorite : Icons.favorite_border,
-              color: Theme.of(context).accentColor,
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
+              icon: Icon(
+                product.isFavourite ? Icons.favorite : Icons.favorite_border,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: product.toggleFavouriteStatus,
             ),
-            onPressed: product.toggleFavouriteStatus,
           ),
           title: Text(
             product.title,
