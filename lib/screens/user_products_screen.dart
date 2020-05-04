e3c138230ffcd6575937e68b475df7e50a0c30c6
HarvsG <11440490+HarvsG@users.noreply.github.com>
2020-05-04 20:04:53 +0100

    implements pull to refresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -10,6 +10,10 @@ class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
   //const UserProductsScreen({Key key}) : super(key: key);
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -26,20 +30,23 @@ class UserProductsScreen extends StatelessWidget {
           ],
         ),
         drawer: AppDrawer(),
-        body: Padding(
-          padding: const EdgeInsets.all(8),
-          child: ListView.builder(
-            itemBuilder: (_, i) => Column(
-              children: <Widget>[
-                UserProductItem(
-                  title: productsData.items[i].title,
-                  imageUrl: productsData.items[i].imageUrl,
-                  id: productsData.items[i].id,
-                ),
-                const Divider()
-              ],
+        body: RefreshIndicator(
+          onRefresh: ()=> _refreshProducts(context),
+                  child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: ListView.builder(
+              itemBuilder: (_, i) => Column(
+                children: <Widget>[
+                  UserProductItem(
+                    title: productsData.items[i].title,
+                    imageUrl: productsData.items[i].imageUrl,
+                    id: productsData.items[i].id,
+                  ),
+                  const Divider()
+                ],
+              ),
+              itemCount: productsData.items.length,
             ),
-            itemCount: productsData.items.length,
           ),
         ));
   }
