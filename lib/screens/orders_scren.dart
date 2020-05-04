f21b6169298f89ceac58b3bf5da53e7013049ce7
George <george@pop-os.localdomain>
2020-05-04 10:46:07 +0100

    fixed type error

--- lib/screens/orders_scren.dart
+++ lib/screens/orders_scren.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/app_drawer.dart';
 import '../widgets/order_list_item.dart';
 import '../providers/orders.dart';
 import 'package:provider/provider.dart';
@@ -9,16 +10,17 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of(context, listen: true);
+    final orderData = Provider.of<Orders>(context, listen: true);
     return Container(
       child: Scaffold(
         appBar: AppBar(
           title: Text('Your Orders'),
         ),
         body: ListView.builder(
-          itemBuilder: (ctx, i) => OrderListItem(orderData[i]),
+          itemBuilder: (ctx, i) => OrderListItem(orderData.orders[i]),
           itemCount: orderData.orders.length,
         ),
+        drawer: AppDrawer(),
       ),
     );
   }
