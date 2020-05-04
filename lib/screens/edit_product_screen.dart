7babd47895c485e52586c4b456f604c8fe38c91a
HarvsG <11440490+HarvsG@users.noreply.github.com>
2020-05-04 14:44:12 +0100

    sets up form entry with focus switching

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,14 +10,119 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  //String _imageUrl;
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    _imageFocusNode.dispose();
+    _imageUrlController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Scaffold(
-         appBar: AppBar(title: Text('Edit Product'),
-         ),
-         body: null,
-       ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Edit Product'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Form(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      hintText: 'e.g Flared Trousers',
+                    ),
+                    textInputAction: TextInputAction.next,
+                    onFieldSubmitted: (_) {
+                      FocusScope.of(context).requestFocus(_priceFocusNode);
+                    },
+                  ),
+                  TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Price (£)',
+                        hintText: '10.99',
+                      ),
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.number,
+                      focusNode: _priceFocusNode,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter a price.';
+                        }
+                        if (double.tryParse(value) == null) {
+                          return 'Please enter a valid number';
+                        }
+                        if (double.parse(value) <= 0.0) {
+                          return 'Please enter a positive number';
+                        }
+                        return null;
+                      },
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context)
+                            .requestFocus(_descriptionFocusNode);
+                      }),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Product Description',
+                      hintText: 'An inspiring pair of 80s memorabelia',
+                    ),
+                    maxLines: 3,
+                    //textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.multiline,
+                    focusNode: _descriptionFocusNode,
+                  ),
+                  Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Container(
+                          width: 100,
+                          height: 100,
+                          margin: EdgeInsets.only(top: 8, right: 10),
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 1, color: Colors.grey)),
+                          child: _imageUrlController.text.isEmpty
+                              ? Center(
+                                  child: Text(
+                                  'Please enter a URL',
+                                  style: TextStyle(color: Colors.grey),
+                                ))
+                              : FittedBox(
+                                  child: Image.network(
+                                  _imageUrlController.text,
+                                  fit: BoxFit.cover,
+                                )),
+                        ),
+                        Expanded(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Image URL',
+                              hintText: 'https://i.imgur.com/uNiQuEiD.jpg',
+                            ),
+                            keyboardType: TextInputType.url,
+                            textInputAction: TextInputAction.done,
+                            controller: _imageUrlController,
+                            focusNode: _imageFocusNode,
+                          ),
+                        ),
+                      ])
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
