17a0067b1ca4f8b21d7f36468e4088309ede3090
HarvsG <11440490+HarvsG@users.noreply.github.com>
2020-05-05 14:20:08 +0100

    async toggFavourites

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_shop_app/models/http_exception.dart';
 //import 'package:provider/provider.dart';
 
 class Product with ChangeNotifier {
@@ -18,9 +22,26 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavouriteStatus() {
+  Future<void> toggleFavouriteStatus() async {
+    final url = 'https://my-shop-app-be8be.firebaseio.com/products/$id.json';
+    //use optimistic updating
+    final oldStatus = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavourite': isFavourite,
+        }),
+      );
+      // http doesnt treat errors on patch as a true error
+      if (response.statusCode >= 400) throw HttpException('Error setting favourite');
+    } catch (e) {
+      // roll back changes
+      isFavourite = oldStatus;
+      notifyListeners();
+      throw e;
+    }
   }
-  
 }
